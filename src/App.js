a590e07fbd0e1141b94a5c498ffdd867362edc4f
shawn <shawncaceres10@gmail.com>
2022-04-20 07:53:06 -0500

    added images and basic layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@
 function App() {
   return (
     <div className="App">
+      <div className="project-wrapper">
+        <div className="header-container">
+          <header className="header">Cute Animals Yay</header>
+        </div>
+        
+      </div>
       
     </div>
   );
