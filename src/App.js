cc0f2724fa9f7d84614f3c9e0500a96af2db0d93
shawn <shawncaceres10@gmail.com>
2022-04-20 11:56:57 -0500

    images added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
+import kitty from './images/kitty.png'
+import pug from './images/pug.png'
+import pusheen from './images/pusheen.png'
+import shiba from './images/shiba.png'
 
 function App() {
+  
+  
   return (
     <div className="App">
       <div className="project-wrapper">
         <div className="header-container">
           <header className="header">Cute Animals Yay</header>
         </div>
-        
+        <div className="img-container"><img src={kitty} alt="kittypic" /></div>
       </div>
       
     </div>
